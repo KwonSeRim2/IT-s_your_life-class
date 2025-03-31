74fc7a92c1e7e839f4cf458953d2435c14011015
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-03-31 19:47:56 +0900

    Add files via upload

--- vue/chapter09/expense-tracker/src/App.vue
+++ vue/chapter09/expense-tracker/src/App.vue
@@ -11,7 +11,57 @@
       :exList="exList"
       :formatNumber="formatNumber"
     ></ExpenseList>
-
+    <div>♟️카테고리별 지출 합계</div>
+    <div>
+      <div
+        class="d-flex"
+        style="
+          background-color: #1e1e1e;
+          margin: 5px;
+          border-radius: 5px;
+          padding: 8px;
+        "
+        v-if="cateTotal.a > 0"
+      >
+        🥤식비: {{ formatNumber(cateTotal.a) }}원
+      </div>
+      <div
+        class="d-flex"
+        style="
+          background-color: #1e1e1e;
+          margin: 5px;
+          border-radius: 5px;
+          padding: 8px;
+        "
+        v-if="cateTotal.b > 0"
+      >
+        🚌교통: {{ formatNumber(cateTotal.b) }}원
+      </div>
+      <div
+        class="d-flex"
+        style="
+          background-color: #1e1e1e;
+          margin: 5px;
+          border-radius: 5px;
+          padding: 8px;
+        "
+        v-if="cateTotal.c > 0"
+      >
+        🛍️쇼핑: {{ formatNumber(cateTotal.c) }}원
+      </div>
+      <div
+        class="d-flex"
+        style="
+          background-color: #1e1e1e;
+          margin: 5px;
+          border-radius: 5px;
+          padding: 8px;
+        "
+        v-if="cateTotal.d > 0"
+      >
+        🧭취미: {{ formatNumber(cateTotal.d) }}원
+      </div>
+    </div>
     <TotalDisplay :total="total" :formatNumber="formatNumber"></TotalDisplay>
   </div>
 </template>
@@ -24,13 +74,12 @@ import TotalDisplay from './components/TotalDisplay.vue';
 import ExpenseList from './components/ExpenseList.vue';
 
 const total = ref(0);
-
 const exList = reactive({ exItems: [] });
 
-const Add = (item, amount) => {
+const Add = (item, amount, id) => {
   total.value += Number(amount);
 
-  exList.exItems.push({ item, amount });
+  exList.exItems.push({ item, amount, id });
 };
 
 const Out = (index) => {
@@ -41,4 +90,22 @@ const Out = (index) => {
 const formatNumber = (total) => {
   return total.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
 };
+
+// 카테고리별 합계를 계산하여 바로 반환하는 computed 함수
+const cateTotal = computed(() => {
+  return {
+    a: exList.exItems
+      .filter((item) => item.id === 1)
+      .reduce((sum, current) => sum + Number(current.amount), 0),
+    b: exList.exItems
+      .filter((item) => item.id === 2)
+      .reduce((sum, current) => sum + Number(current.amount), 0),
+    c: exList.exItems
+      .filter((item) => item.id === 3)
+      .reduce((sum, current) => sum + Number(current.amount), 0),
+    d: exList.exItems
+      .filter((item) => item.id === 4)
+      .reduce((sum, current) => sum + Number(current.amount), 0),
+  };
+});
 </script>
