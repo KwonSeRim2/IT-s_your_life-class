80208cfc6026fe10dce8544a007789ded6b843fb
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-04-03 17:47:35 +0900

    0403

---
+++ vue/chapter12/todolist-app-router/src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="m-3">
+    <h3>존재하지 않는 경로</h3>
+    <p>요청 경로 : {{ currentRouter.path }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const currentRouter = useRouter();
+</script>
