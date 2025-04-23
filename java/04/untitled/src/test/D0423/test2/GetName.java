bb9a3a513d5de0fff11659322dfeb7e1951d8c84
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-04-23 21:54:33 +0900

    0423 - test2 add

--- java/04/untitled/src/test/D0423/test2/GetName.java
+++ java/04/untitled/src/test/D0423/test2/GetName.java
@@ -10,7 +10,7 @@ public class GetName {
 
     public void nameCheck(String name) throws InvalidNameException {
         if (Pattern.matches("^[가-힣]{2,10}$", name)) {
-            System.out.println("(다음 단계로 진행합니다!)");
+            //System.out.println("(다음 단계로 진행합니다!)");
         } else {
             throw new InvalidNameException("😡 탈락! 잘못된 이름입니다: " + name);
         }
